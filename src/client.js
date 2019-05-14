8cca3687915eaa986bbf233dc889c28ccaac4a08
Andrew Smith <andrew.smith@wwinc.com>
2019-05-14 11:30:26 -0500

    Added a skeleton of a v2 extension package.

---
+++ src/client.js
@@ -0,0 +1,23 @@
+'use strict';
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const promisify = require('util.promisify');
+const fetch = require('node-fetch');
+
+/**
+ * @param {Mozaik} mozaik
+ */
+const client = mozaik => {
+  // mozaik.loadApiConfig(config)
+
+  const apiCalls = {
+    download: ({ url }) => {
+    },
+  };
+
+  return apiCalls;
+};
+
+module.exports = client;
