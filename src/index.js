88a2cdfe4abce9cb4396b7b0cb566871a9692254
Andrew Smith <andrew.smith@wwinc.com>
2019-05-16 12:30:01 -0500

    Restored the Channel and Since widgets.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { default as HelloWorld } from './ui/components/HelloWorld';
 import { default as Pulse } from './ui/components/Pulse';
+import { default as Channel } from './ui/components/Channel'
+import { default as Since } from './ui/components/Since'
+
 export default {
   HelloWorld,
   Pulse,
-  Impulse
+  Impulse,
+  Since,
+  Channel
 };
