cc093e5971563afdb5dfa4d30704212298e9f483
Andrew Smith <andrew.smith@wwinc.com>
2019-05-20 13:40:39 -0500

    Deleting of files and removing of formatting was causing a crash, temporarily removed.

--- src/client.js
+++ src/client.js
@@ -391,14 +391,14 @@ module.exports =  mozaik => {
             }
 
             // Delete old files async (not interested in outcome)
-            deleteFiles(path.join(publicDir, tempDirName), maxImageAge);
+            //deleteFiles(path.join(publicDir, tempDirName), maxImageAge);
 
             // Remove Slack syntax to make outcome more readable
-            const removeFormat = getFormatRemover({
-              users:    users,
-              channels: channels
-            });
-            message.text  = removeFormat(message.text || '');
+            // const removeFormat = getFormatRemover({
+            //   users:    users,
+            //   channels: channels
+            // });
+            //message.text  = removeFormat(message.text || '');
             message.text  = replaceEmojis(message.text);
             message.image = image ? path.relative(publicDir, image) : null;
             message.text  = image ? _.get(message, 'file.initial_comment.comment', null) || message.file.title : message.text;
